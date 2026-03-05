1a05454814b933e7d4a6ce1a5a0cda0ecccdfd64
zong <69517696+caizongxun@users.noreply.github.com>
2026-03-05 23:17:42 +0800

    fix: 新增 v7-v13 導出

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,10 +1,17 @@
-# 导出所有版本
+# 導出所有版本
 from . import v1
 from . import v2
 from . import v3
 from . import v4
 from . import v5
 from . import v6
+from . import v7
+from . import v8
+from . import v9
+from . import v10
+from . import v11
+from . import v12
+from . import v13
 
 AVAILABLE_VERSIONS = {
     'v1': v1,
@@ -13,4 +20,11 @@ AVAILABLE_VERSIONS = {
     'v4': v4,
     'v5': v5,
     'v6': v6,
+    'v7': v7,
+    'v8': v8,
+    'v9': v9,
+    'v10': v10,
+    'v11': v11,
+    'v12': v12,
+    'v13': v13,
 }
